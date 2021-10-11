239799f65fb77f1a2d91e2f5b44d30dcbc3f93af
sljssims <sljssims@gmail.com>
2021-10-11 08:10:25 -0500

    updates

--- Anaysis Projects Folder/Queries/queries.sql
+++ Anaysis Projects Folder/Queries/queries.sql
@@ -78,15 +78,15 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no;
 
 -- Create New Table Refactor Previous Query (Include Into and Where to make a table)(Left Join)
-SELECT ri.emp_no,
-    ri.first_name,
-	ri.last_name,
-    de.to_date
-INTO current_emp	
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
-WHERE de.to_date = ('9999-01-01');	
+SELECT      ri.emp_no,
+            ri.first_name,
+	        ri.last_name,
+            de.to_date
+INTO        current_emp	
+FROM        retirement_info as ri
+LEFT JOIN   dept_emp as de
+ON          ri.emp_no = de.emp_no
+WHERE       de.to_date = ('9999-01-01');	
 
 
 -- Employee count by department number
@@ -129,53 +129,53 @@ AND (de.to_date = '9999-01-01');
 --Management Information--------------
 -- Generate a list of managers for each department, depart number, name, managers employee number, 
 -- first and last name, and starting and ending dates
-SELECT  dm.dept_no,
-        d.dept_name,
-        dm.emp_no,
-        ce.last_name,
-        ce.first_name,
-        dm.from_date,
-        dm.to_date
-INTO manager_info
-FROM dept_manager AS dm
-INNER JOIN departments AS d
-ON (dm.dept_no = d.dept_no)
-INNER JOIN current_emp AS ce
-ON (dm.emp_no = ce.emp_no);
+SELECT      dm.dept_no,
+            d.dept_name,
+            dm.emp_no,
+            ce.last_name,
+            ce.first_name,
+            dm.from_date,
+            dm.to_date
+INTO        manager_info
+FROM        dept_manager AS dm
+INNER JOIN  departments AS d
+ON          (dm.dept_no = d.dept_no)
+INNER JOIN  current_emp AS ce
+ON          (dm.emp_no = ce.emp_no);
 
 --Department Retirees-----------------
 --An updated current_empl list that includes the employee departments too
-SELECT ce.emp_no,
-	ce.first_name,
-	ce.last_name,
-	d.dept_name
-INTO dept_info
-FROM current_emp AS ce
-INNER JOIN dept_emp AS de
-ON (ce.emp_no = de.emp_no)
-INNER JOIN departments AS d
-on (de.dept_no = d.dept_no);
+SELECT      ce.emp_no,
+	        ce.first_name,
+	        ce.last_name,
+	        d.dept_name
+INTO        dept_info
+FROM        current_emp AS ce
+INNER JOIN  dept_emp AS de
+ON          (ce.emp_no = de.emp_no)
+INNER JOIN  departments AS d
+on          (de.dept_no = d.dept_no);
 
 -- Department Retirees of Sales--------
-SELECT e.emp_no, e.first_name, e.last_name, d.dept_name
-INTO sales_info
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN departments as d
-ON (de.dept_no = d.dept_no)
-WHERE (d.dept_no ='d007')
-AND (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT      e.emp_no, e.first_name, e.last_name, d.dept_name
+INTO        sales_info
+FROM        employees as e
+INNER JOIN  dept_emp as de
+ON          (e.emp_no = de.emp_no)
+INNER JOIN  departments as d
+ON          (de.dept_no = d.dept_no)
+WHERE       (d.dept_no ='d007')
+AND         (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND         e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Department Retirees of Sales and Development --------
-SELECT e.emp_no, e.first_name, e.last_name, d.dept_name
-INTO sales_dev
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN departments as d
-ON (de.dept_no = d.dept_no)
-WHERE (d.dept_no IN ('d007', 'd005'))
-AND (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT      e.emp_no, e.first_name, e.last_name, d.dept_name
+INTO        sales_dev
+FROM        employees as e
+INNER JOIN  dept_emp as de
+ON          (e.emp_no = de.emp_no)
+INNER JOIN  departments as d
+ON          (de.dept_no = d.dept_no)
+WHERE       (d.dept_no IN ('d007', 'd005'))
+AND         (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND         (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
