c210a0d8c1e3396c1fc6d04534e00174b46ec96b
sljssims <sljssims@gmail.com>
2021-10-10 20:06:55 -0500

    uniquetitlescsv

--- Anaysis Projects Folder/Queries/Employee_Database_Challenge.sql
+++ Anaysis Projects Folder/Queries/Employee_Database_Challenge.sql
@@ -1,4 +1,5 @@
 --Chapter 7 Challenge
+------Filter Retirement Titles----
 SELECT  e.emp_no, 
         e.first_name, 
         e.last_name,
@@ -12,3 +13,12 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC; 
 SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, 
+                    last_name, title, 
+                    from_date. to_date
+INTO                unique_titles
+FROM                retirement_titles AS rt
+ORDER BY            e.emp_no , rt.to_date DESC;
+SELECT * FROM       unique_titles;
